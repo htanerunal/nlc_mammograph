a10105285a88af7f7e92040d5399f36a4323ea8b
Jetkite Inc <jetkiteinc@Jetkite-iMac.local>
2021-11-26 09:32:24 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/nlc_mammograph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nlc_mammograph.dir/main.cpp.o"
+  "nlc_mammograph"
+  "nlc_mammograph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nlc_mammograph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
